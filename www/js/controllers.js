de65be43ede226d6cecd31f8b37920398e8fc7f9
sandrooliveira <sandroaugusto67@gmail.com>
2015-12-02 19:52:44 -0200

    Daily report

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -69,9 +69,82 @@ angular.module('app.controllers', ['ionic','highcharts-ng'])
   $scope.user = user;
 })
 
-.controller('dailyReportCtrl', function($scope) {
+.controller('dailyReportCtrl', function($scope, $http, $localstorage) {
 
-})
+  //init controller
+  var user = $localstorage.getObject('user');
+
+  function listMonths($scope, $http, userCPF) {
+    var monthNames = ["","January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
+
+    var months = [];
+
+    $http.get('http://smart-water.tk/api/report/getMonthsByUser/'+userCPF).success(function(data) {
+      for (var i in data){
+        months.push({'value' : data[i].month+"/"+data[i].year, 'description' : monthNames[data[i].month]+" "+data[i].year});
+      }
+      $scope.months = months;
+    });
+  }
+
+  function setDailyCharts($scope, $http, userCPF, year, month){
+    var monthNames = ["","January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+
+    monthTitle = monthNames[month] +" "+year;
+    $http.get('http://smart-water.tk/api/report/daily/'+userCPF+"/"+year+"/"+month).success(function(months) {
+      $scope.dailyCharts = {
+        options: {
+          title: {
+            text: 'Daily consumption'
+          },
+          subtitle: {
+            text: monthTitle
+          }
+        },
+        xAxis: {
+          categories: months.categories,
+          crosshair: true
+        },
+        yAxis: {
+          min: 0,
+          title: {
+            text: 'Flow (Liters)'
+          },
+          plotLines: [{
+            label: {
+              text: 'Average (' + months.average.toFixed(3) + ' liters)',
+              align: 'left'
+            },
+            dashStyle: 'dash',
+            color: 'green',
+            value: months.average,
+            width: '2',
+            zIndex: 2
+          }]
+        },
+        tooltip: {
+          headerFormat: '<span style="font-size:10px">Day {point.key}</span><table>',
+          pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+          '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+          footerFormat: '</table>',
+          shared: true,
+          useHTML: true,
+          valueSuffix: 'liters'
+        },
+        plotOptions: {
+          column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+          }
+        },
+        series: [{
+          name: 'Water',
+          data: months.series
+        }]
+      }
+    });
+  }
 
 .controller('monthlyReportCtrl', function($scope, $http, $localstorage) {
 
